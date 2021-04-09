cbc8349ad45a9e653de26a31881ed4c0d13171a2
Archipipe <77447627+Archipipe@users.noreply.github.com>
2021-04-09 16:06:51 +0300

    lesson1

---
+++ lessons/lesson01.py
@@ -0,0 +1,15 @@
+car1 = "Tototpo"
+car2 = 1
+car3 = 2.4
+car = ["Да", "Net", "Konechno", "AbhChu", "da"]
+
+
+# car.append(el) - Добавляет в конец массива эл. el
+# car.pop(i) - Удаляет из массива эл с индексом i или просто посл. эл
+# car.remove(el) - Удаляет из массива эл с соот. зн el
+# car.clear() - Очищает массив
+# car.count(el) - Находит кол-во эл. el
+# car.index(el) - Индекс эл. соот. зн. el
+# car.reverse() - Перев. массив
+# car.sort() - Сортирует массив
+# car.copy() - копирует
