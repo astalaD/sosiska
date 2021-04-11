50e1353dcdafb265f4c169a7a0b0abea581c3f86
Archipipe <77447627+Archipipe@users.noreply.github.com>
2021-04-11 20:06:54 +0300

    lesson2-3

---
+++ lessons/lesson02.py
@@ -0,0 +1,55 @@
+# for i in range(5):
+#     print(i)
+
+# for i in range(0,11):
+#     if i % 2== 0:
+#         print(i)
+
+# array = [1,"dfgdfgfd","323",7,42]
+# for i in array:
+#     if type(i) == int:
+#         print(i)
+
+# obj = {"dfgdfg" : "фыыа", 2 : "dadada"}
+# for i in obj:
+#     print(obj[i])
+
+# a = 0
+# while True:
+#     print(a)
+#     a+=1
+
+# arr = ["Игнат", "Влад", "Саня", "Ихватит"]
+# counter = 0
+# for q in arr:
+#     a = 0
+#     for i in q:
+#         if i == "н":
+#             a+=1
+#             break
+#     if a!=0:
+#         counter+=1
+# print(counter)
+
+# a = 20  
+# while a > 5:
+#     print(a)
+#     a-=3
+
+# arr = [1,2,4,5]
+# for i in arr:
+#     if i == 3:
+#         print("Есть цифра 3")
+#         break
+# else:
+#     print("Нихера не найдено")
+
+# n = 0
+# while n < 20:
+#     if n == 3:
+#         n +=1
+#         continue
+#     print(n)
+#     if n == 5:
+#         break
+#     n += 1
---
+++ lessons/lesson03.py
@@ -0,0 +1,19 @@
+# def Da(x):
+#     return x - 15
+
+# a = Da(20)
+# print(a)
+
+# def Da(x = 20):
+#     return x - 15
+# a = Da(20)
+# print(a)
+
+# def func(x,y):
+#     return x**2 + y**2
+# func = lambda x,y : x**2 + y**2
+
+
+# def func(x,y,z=7):
+#     return x+y+z
+# print(func(y=12,x=333))
